ec682b462fe35f552fb79f00237392b72504edb2
Kyle <kawigdd@gmail.com>
2022-12-03 18:23:32 -0500

    track app version

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import './index.scss';
 
 ReactGA.initialize(import.meta.env.VITE_GA, { debug: import.meta.env.DEV });
 ReactGA.pageview('/');
+ReactGA.set({
+  appId: 'casbingo',
+  appName: 'CS A Bingo',
+  appVersion: import.meta.env.VITE_VERSION,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import pkg from './package.json';
+
+const version = pkg.version;
+const revision = execSync('git describe --long --always --dirty=-dirty')
+  .toString()
+  .trim();
+
+process.env.VITE_VERSION = `v${version}.${revision}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
