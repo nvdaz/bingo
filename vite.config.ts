ed02796d0a609e8224375ec55faf5479c4544abf
Kyle <kawigdd@gmail.com>
2022-12-08 17:21:14 -0500

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import mdx from '@mdx-js/rollup';
+import { execSync } from 'child_process';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import pkg from './package.json';
@@ -18,6 +19,7 @@ process.env.VITE_VERSION = `v${version}.${revision}`;
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({ registerType: 'autoUpdate' }),
     svgr(),
     mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] }),
   ],
